f56a913942e87c0988c376c649a58cf78c155c0f
Jonathan Romero <n.bjhon@gmail.co>
2020-09-10 22:24:03 -0500

    mostrando el articulo en la web

--- myblog/theblog/urls.py
+++ myblog/theblog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from . import views
+from .views import HomeView, ArticleDetailView
 
 urlpatterns = [
-    path('', views.index, name="index"),
-    
+    path('', HomeView.as_view(), name="home"),
+    path('articulo/<pk>', ArticleDetailView.as_view(), name='detalle_articulo')
 ]
--- myblog/theblog/views.py
+++ myblog/theblog/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
-def index(request):
-    return render(request, 'index.html', {})
+class HomeView(ListView):
+    model = Post
+    template_name = 'index.html'
+
+class ArticleDetailView(DetailView):
+    model = Post
+    template_name = 'detalle_articulo.html'
