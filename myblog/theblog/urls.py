01f006530bea15c27c35c64a1e4569055eb0b36a
Jonathan Romero <n.bjhon@gmail.co>
2020-09-12 23:34:11 -0500

    añadiendo eliminacion de post

--- myblog/theblog/urls.py
+++ myblog/theblog/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import HomeView, ArticleDetailView, AddPostView, UpdatePostView
+from .views import HomeView, ArticleDetailView, AddPostView, UpdatePostView, DeletePostView
 
 urlpatterns = [
     path('', HomeView.as_view(), name="home"),
     path('articulo/<pk>', ArticleDetailView.as_view(), name='detalle_articulo'),
     path('add_post/', AddPostView.as_view(), name='añadir_post'),
     path('articulo/editar/<int:pk>', UpdatePostView.as_view(), name='actualizar_post'),
-
+    path('articulo/<int:pk>/eliminar', DeletePostView.as_view(), name='eliminar_post'),
 
 ]
--- myblog/theblog/views.py
+++ myblog/theblog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 from .forms import PostForm, EditForm
+from django.urls import reverse_lazy
 
 class HomeView(ListView):
     model = Post
@@ -21,3 +22,8 @@ class UpdatePostView(UpdateView):
     form_class = EditForm
     template_name = 'actualizar_post.html'
     #fields = ['titulo', 'titulo_tag', 'articulo']
+
+class DeletePostView(DeleteView):
+    model = Post
+    template_name = 'eliminar_post.html'
+    success_url = reverse_lazy('home')
