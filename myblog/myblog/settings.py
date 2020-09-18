8cd8d2547ae6f25384e6a161c12b9e5e3253b10f
Jonathan Romero <n.bjhon@gmail.co>
2020-09-17 20:40:06 -0500

    añadiendo static_root

--- myblog/myblog/settings.py
+++ myblog/myblog/settings.py
@@ -120,12 +120,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
