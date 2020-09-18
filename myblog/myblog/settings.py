a480a1de81f5c01594a73fe173b43ffb32d82295
Jonathan Romero <n.bjhon@gmail.co>
2020-09-17 20:36:52 -0500

    añadiendo static_root

--- myblog/myblog/settings.py
+++ myblog/myblog/settings.py
@@ -124,6 +124,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = (
